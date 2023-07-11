9818f26cf352a10a7759af4553cbf743dc8a48dd
ViridianMelody <mlove@anaconda.com>
2023-07-11 09:08:50 -0600

    Remove line breaks from run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -2,14 +2,4 @@
 cd test
 :: test_audio_examples uses an uninstalled local folder ("examples");
 :: avoid test_text_examples due to cycle since torchtext depends on torchdata
-pytest -v --ignore=test_audio_examples.py ^
---ignore=test_text_examples.py ^
--k "not (_not_a_real_test ^
-or test_fsspec_memory_list ^
-or test_elastic_training_dl1_backend_gloo ^
-or test_elastic_training_dl2_backend_gloo ^
-or test_fsspec_io_iterdatapipe ^
-or test_online_iterdatapipe ^
-or test_gdrive_iterdatapipe ^
-or test_list_files ^
-or test_list_files_with_filter_mask)"
+pytest -v --ignore=test_audio_examples.py --ignore=test_text_examples.py -k "not (_not_a_real_test or test_fsspec_memory_list or test_elastic_training_dl1_backend_gloo or test_elastic_training_dl2_backend_gloo or test_fsspec_io_iterdatapipe or test_online_iterdatapipe or test_gdrive_iterdatapipe or test_list_files or test_list_files_with_filter_mask)"
